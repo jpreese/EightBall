ad19cb77948fefc9819c9943ef10e79f1e703cdd
John Reese <jpreese@mtu.edu>
2017-11-12 17:31:05 -0500

    Initial commit

---
+++ PackageShipper/PriceCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PackageShipper
+{
+    public class PriceCalculator
+    {
+        public int Calculate(string messageToSend)
+        {
+            var price = messageToSend.Length;
+
+            if(messageToSend.Length >= 5)
+            {
+                price += 5;
+            }
+
+            if(DateTime.Now.DayOfWeek == DayOfWeek.Monday)
+            {
+                price += 100;
+            }
+
+            if(messageToSend.Contains("secret"))
+            {
+                price = 0;
+            }
+
+            return price;
+        }
+    }
+}
---
+++ PackageShipper/ShippingApplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PackageShipper
+{
+    public class ShippingApplication
+    {
+        public void Ship(string messageToSend)
+        {
+            var priceCalculator = new PriceCalculator();
+            var price = priceCalculator.Calculate(messageToSend);
+
+            Console.WriteLine($"Your message has been shipped! It costed: {price}");
+        }
+    }
+}
